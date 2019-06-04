f34ec972a55910f632be845f91ee5d24353ddda9
andyWang <295268319@qq.com>
2019-06-04 20:22:27 +0800

    增加QQ登录的两种方式

---
+++ adapter/src/main/java/com/andy/design/pattern/login/SignInWithQQ.java
@@ -0,0 +1,33 @@
+package com.andy.design.pattern.login;
+
+import com.andy.design.pattern.Result;
+
+
+/**
+ * <p>QQ登录</p>
+ * 使用继承，不改变原有逻辑
+ * @author AndyWang QQ:295268319
+ * @date 2019/6/4 0004 20:10
+ */
+public class SignInWithQQ extends SignInService {
+
+    /**
+     * QQ登录
+     * @param openId
+     * @return
+     */
+    public Result loginForQQ(String openId){
+        return loginForRegist(openId,null);
+    }
+
+    /**
+     * 登录
+     * @param userName
+     * @param password
+     * @return
+     */
+    public Result loginForRegist(String userName, String password){
+        super.regist(userName,null);
+        return super.login(userName,null);
+    }
+}
