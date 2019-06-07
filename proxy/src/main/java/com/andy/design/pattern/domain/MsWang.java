6829a27817d567c3df67170f1968d6f325c57074
andyWang <295268319@qq.com>
2019-06-07 11:28:31 +0800

    增加proxy实现

---
+++ proxy/src/main/java/com/andy/design/pattern/domain/MsWang.java
@@ -0,0 +1,12 @@
+package com.andy.design.pattern.domain;
+
+/**
+ * wang先生
+ */
+public class MsWang implements Person{
+
+    @Override
+    public void findJob() {
+        System.out.println("找工作，薪水高，加班不多就行");
+    }
+}
